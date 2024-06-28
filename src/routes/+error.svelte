1de8ae99cf6019159ae731331624f88ae581715e
Bismark <agyarebismark6@gmail.com>
2024-06-28 23:18:11 +0000

    EDITED CONTENT

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,18 @@
-<h1>Error page</h1>
+<style>
+
+.main{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    align-self: center;
+    align-items: center;
+    height: max-content;
+}
+    h1{
+    font-size: 34px;
+    }
+</style>
+<div class="main">
+
+    <h1>Error pge</h1>
+</div>
