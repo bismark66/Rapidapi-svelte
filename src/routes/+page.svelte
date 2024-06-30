aa3cc8cd2148c52af4147af2a0179f1d70b4dd57
Bismark <agyarebismark6@gmail.com>
2024-06-30 17:32:41 +0000

    tested react statements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts"> 
     import Button from '$lib/button.svelte';
     import Card from '$lib/card.svelte';
   
@@ -6,11 +6,19 @@
     function handleButtonClick() {
       alert('Button clicked!');
     }
-  </script>
+
+
+export let nam:string="heello"
+console.log(nam, " world")
+let count = 0;
+
+// reactive statement, used to triger a change from a different state unto what is passed to, it also run after all other scripts
+$: doubled = count * 2;
+
+</script>
 
 <!-- <h1>Welcome to SvelteKit this is Bismark</h1> -->
 <p>Visit <a href="/about">kit.svelte.dev</a> to read the document</p>
-
 <Button variant="danger" onClick={handleButtonClick}>Click</Button>
 
 
